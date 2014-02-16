d9089f4fa317308900d0c8431ae4268a503f1f66
Stephen Turban <Stephen@Stephens-MacBook-Pro-2.local>
2014-02-16 02:28:29 -0500

    YAAAAAAAA. ADDED LOG-IN. JAVA SCRIPTgit commitgit commit

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -16,14 +16,59 @@
  */
 
 module.exports = {
-    
-  
+ 
+	// request is what is requested, responsre is what you respond. and next 
+	// is what you would do if you didn't want to do anything specific  
+	login: function(req, res, next) { 
+
+		var email = req.param("email")
+		var password = req.param ("password")
+
+		if (!email || !password) { 
+			return res.json({ 	
+				error: "Give me a password! NOW!"
+			})
+		}
+		// this finds a user, where we are looking for a user whose email address matches
+		// the email address that we have give through login 
+		// after you are /done then can either return an error or  a user 
+		User.findOne({ email: email}).done(function(err, user)) { 
+				// error is only something that is returned if something 
+				if (err) { 
+					res.send(500, {error: "DB Error"}); 
+				} else {  
+
+					if(user) {
+						// Validate that the password is true, and then we can directly compare two functions 
+						// can either return three things an error a result or a password 
+						bcrypt.compare(password, user.encryptedPassword, function(err, result)) { 
+
+							if (result) { 
+								req.session.user = user; 
+								res.redirect('/');	
+							}
 
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to UserController)
-   */
+							else { 
+
+								res.view({
+									error: "Wrong Password"
+								});		
+
+
+							}	
+						}
+					}
+					else {
+						res.view({
+							error: "Email not found"
+						});
+					}
+				}
+		}
+	}
+
+
   _config: {}
 
   
